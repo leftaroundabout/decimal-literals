412925126b19c4ae93559514376baf7237d54b65
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-21 17:00:23 +0200

    Initial commit.

---
+++ Numeric/Literals/Decimal.hs
@@ -0,0 +1,22 @@
+-- |
+-- Module      : Numeric.Literals.Decimal
+-- Copyright   : (c) Justus Sagemüller 2017
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsagemue $ uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+module Numeric.Literals.Decimal where
+
+
+-- | A type whose 'Fractional' instance gives a somewhat reliable indication whether
+--   a value was actually defined as an integer or a ratio, or as a decimal-fraction
+--   literal. This is useful to know for a type that supports both exact fraction
+--   values and more floating-point-like / physical values; it allows avoiding
+--   things like @0.524@ showing up as @589971551185535/1125899906842624@, or conversely
+--   @7/23@ as @0.30434782608695654@. Both of these scenarios are quite awkward.
+data FractionalLit = ExactRatio Rational
+                   | DecimalFraction String
+
