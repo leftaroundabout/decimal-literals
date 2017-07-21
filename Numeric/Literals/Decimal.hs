c4fdd89cc697f546d1c5515a7bbfd661fa6e3a3f
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-21 17:17:51 +0200

    Basic structure of the literal-type, and a `Num` instance.

--- Numeric/Literals/Decimal.hs
+++ Numeric/Literals/Decimal.hs
@@ -10,6 +10,8 @@
 
 module Numeric.Literals.Decimal where
 
+import Data.Ratio
+
 
 -- | A type whose 'Fractional' instance gives a somewhat reliable indication whether
 --   a value was actually defined as an integer or a ratio, or as a decimal-fraction
@@ -18,5 +20,17 @@ module Numeric.Literals.Decimal where
 --   things like @0.524@ showing up as @589971551185535/1125899906842624@, or conversely
 --   @7/23@ as @0.30434782608695654@. Both of these scenarios are quite awkward.
 data FractionalLit = ExactRatio Rational
-                   | DecimalFraction String
+                   | DecimalFraction {
+                       decimalMantissa :: Integer
+                     , decimalExponent :: Int }
+
+instance Eq FractionalLit where
+  ExactRatio r₀ == ExactRatio r₁  =  r₀==r₁
+  DecimalFraction m₀ e₀ == DecimalFraction m₁ e₁  =  m₀==m₁ && e₀==e₁
+  ExactRatio r == DecimalFraction m e
+   | e>0        = ExactRatio r == DecimalFraction (m*10^e) 0
+   | (ers,0) <- (10^(-e)) `divMod` denominator r
+                =  m == numerator r*ers
+   | otherwise  =  False
+  n == m  =  m == n
 
