09c51756951de44688e330f07d73f170f09434e1
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2017-07-22 13:12:13 +0200

    Add test suite.

---
+++ test/tasty/test.hs
@@ -0,0 +1,44 @@
+-- |
+-- Module      : Main
+-- Copyright   : (c) Justus Sagemüller 2017
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+module Main where
+
+import Numeric.Literals.Decimal
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+
+
+main = defaultMain tests
+
+
+reshowTest :: FractionalLit -> String -> TestTree
+reshowTest n str = testCase str $ show n @?= str
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ testGroup "Re-showing literals"
+     [ reshowTest                  4 "4"
+     , reshowTest             142125 "142125"
+     , reshowTest            125.278 "125.278"  -- at the moment: "1.25278e2"
+     , reshowTest                0.2 "0.2"
+     , reshowTest               0.04 "0.04"
+     , reshowTest              0.008 "8e-3"
+     , reshowTest              1e-52 "1e-52"
+     , reshowTest    1.167925373e-12 "1.167925373e-12"
+     , reshowTest              0.254 "0.254"  -- at the moment: "2.54e-1"
+     , reshowTest (-1.167925373e-12) "-1.167925373e-12"
+     , reshowTest          (-142125) "-142125"
+     ]
+  ]
+
+
+
